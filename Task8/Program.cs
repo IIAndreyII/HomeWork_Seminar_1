c6a9956f0d716c9dc45efd573ad34101f2a3a2e9
unknown <volk.3894@yandex.ru>
2022-08-24 22:40:09 +0300

    add Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+if (n > 1)
+{
+    while(count <= n )
+    {
+        if(count % 2 == 0)
+        {
+            Console.Write(count);
+            Console.Write(" ");
+        }
+        count++;
+    }
+
+
+}
+if (n < 1)
+{
+    while(n <= 1 )
+    {
+        if(n % 2 == 0)
+        {
+            Console.Write(n);
+            Console.Write(" ");
+        }
+        n++;
+    }
+
+
+}
+
